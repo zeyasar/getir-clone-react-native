676a0de6a595ebc2c49d0ff31bdc69633c8808a4
zeynep <zynpysr67@gmail.com>
2023-09-29 13:38:34 +0300

    deep linking added

--- App.tsx
+++ App.tsx
@@ -3,11 +3,30 @@ import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigators/RootNavigator";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
+import * as Linking from 'expo-linking';
+
+
+const prefix = Linking.createURL('/')
+const universal = Linking.createURL('http://getir.example.com')
 
 export default function App() {
+  const linking= {
+    prefixes:[prefix,universal],
+    config: {
+      screens: {
+        Search: {
+          screens:{
+            CartScreen:{
+            path:"cartScreen",
+            }
+          }
+        }
+      }
+    }
+  }
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer linking={linking}>
         <RootNavigator />
       </NavigationContainer>
     </Provider>
